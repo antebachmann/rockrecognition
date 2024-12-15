5d49b0622e081dba310ade700706ab7c89626c38
touria17 <elmouttaki.touria@gmail.com>
2024-12-15 22:00:35 +0100

    funtion for early fusion

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,1 @@
-from .data_preparation import prepare_yolo_annotations, copy_files
+from .data_preparation import prepare_yolo_annotations, copy_files, early_fusion, normalize
--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,5 +1,9 @@
 import os
 import shutil
+from PIL import Image
+import numpy as np
+from tifffile import tifffile as tiff
+
 
 def prepare_yolo_annotations(dataset, output_dir):
     """
@@ -31,4 +35,30 @@ def copy_files(file_list, src_img, src_ann, dest_img, dest_ann):
     for file_name in file_list:
         shutil.copy(os.path.join(src_img, f"{file_name}.tif"), dest_img)
         shutil.copy(os.path.join(src_ann, f"{file_name}.txt"), dest_ann)
+
+
+def normalize(img):
+    if img.dtype != np.uint8:
+        img = ((img - img.min()) / (img.max() - img.min()) * 255).astype(np.uint8)
+    return img
+
+
+def early_fusion(si_path, ss_path, hs_path, output_path):
+    si_img = tiff.imread(si_path)  # Image RGB (3 canaux)
+    ss_img = tiff.imread(ss_path)  # Image Surface 3D (1 canal)
+    hs_img = tiff.imread(hs_path)  # Image Hillshade (1 canal)
+
+    si_img = normalize(si_img)
+    ss_img = normalize(ss_img)
+    hs_img = normalize(hs_img)
+
+    if si_img.shape[:2] != ss_img.shape[:2] or si_img.shape[:2] != hs_img.shape[:2]:
+        raise ValueError(f"Les dimensions ne correspondent pas pour {si_path}")
+
+    ss_img = np.expand_dims(ss_img, axis=2)  
+    hs_img = np.expand_dims(hs_img, axis=2)
+    fused_image = np.dstack((si_img, ss_img, hs_img))  
+
+    tiff.imwrite(output_path, fused_image)
+    print(f"saved fusion for an img: {output_path}")
 
